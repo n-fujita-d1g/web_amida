801209b9114cb2672c5a90661678cf0fada8efac
n-fujita-d1g <n-fujita@daiichi-g.com>
2024-12-25 15:54:02 +0900

    feat client install prettier

---
+++ site/.prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  $schema: 'https://json.schemastore.org/prettierrc',
+  semi: false,
+  tabWidth: 2,
+  singleQuote: true,
+  printWidth: 100,
+  htmlWhitespaceSensitivity: 'ignore',
+  trailingComma: 'all',
+}
